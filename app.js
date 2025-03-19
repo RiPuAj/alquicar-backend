5b7d1ec20370fed78596414b21549e3749d7bd87
Javier Pulido <javipulido15@gmail.com>
2025-03-19 03:53:52 +0000

    Created routes

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-import express from 'express'
-import { UserModel } from './models/mysql/user.js';
+import express from 'express';
+import dotenv from 'dotenv';
+import { userRouter } from './routes/users.js';
 
-const PORT = process.env.PORT ?? 3000;
+
+dotenv.config({path: './.env'});
 
 const app = express();
 app.use(express.json());
 app.disable('x-powered-by');
+
+app.use('/users', userRouter);
 
 
 app.use((req, res) => {
     res.status(404).send('<h1>404 Not Found</h1>');
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
     }
 );
