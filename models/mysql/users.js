5d4355704706d77c30ce97fe656378cc3debee73
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-04-10 19:04:14 +0100

    ADD getByEmail()

--- models/mysql/users.js
+++ models/mysql/users.js
@@ -37,7 +37,17 @@ export class UserModel{
         
     }
 
-  
+    static async getByEmail({email}){
+        try{
+            const [users, tableInfo] = await conn.query(
+                'SELECT BIN_TO_UUID(id) AS id, name, email, address, phone, role, dni FROM users WHERE email = ?',  [email]);
+            return users;
+        }catch(e){
+            // TODO Manejar error
+            console.log(e);
+            throw new DatabaseError('Error getting user');
+        }
+    }
     
     static async create({input}){
 
