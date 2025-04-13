de20bdc1b949761e7f363f93e1a84d61f9ef20f7
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-04-13 22:32:49 +0100

    UPDATE README

--- README.md
+++ README.md
@@ -7,16 +7,25 @@ npm run dev
 ```
 
 ## Dependencias:
+- Node.js, EXPRESS
 - Base de datos activa en el puerto para desarrollo local 3006
 - Archivo de configuración de la base de datos en *[./alquicarDB.sql](./alquicarDB.sql)*
 - dependencias de software instalables con: 
 ```bash 
 npm install
 ```
-
+### Servidor SQL
+- Para que este proyecto se ejecute correctamente es necesario tener un servidor mysql activo en el puerto 3006.
+- Este servidor será accedido a la cuenta **root** **SIN** contraseña.
+- Si se desease se podria utilizar una base de datos mariadb en el mismo puerto en la rama deploy-mariadb con la configuración de la DB que se encuentra en dicha rama
+- Enlaces a [mysql server](https://dev.mysql.com/downloads/mysql/) y [mysql workbench](https://dev.mysql.com/downloads/workbench/)
 ## Métodos generales de acceso:
+- En [api.http](./api.http) se encuentran ejemplos de acceso
 ### User:
-
+- GET
+- POST
+- PATCH
 ### Vehicle:
-
+- GET
 ### Reservation:
+- GET
---
+++ alquicarDB.sql
@@ -0,0 +1,129 @@
+CREATE DATABASE alquicardb;
+
+USE alquicardb;
+
+DROP TABLE IF EXISTS payments;
+DROP TABLE IF EXISTS incidences;
+DROP TABLE IF EXISTS reservations;
+DROP TABLE IF EXISTS vehicles;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users(
+	id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(72) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone VARCHAR(9) NOT NULL UNIQUE,
+    role ENUM('admin', 'user') NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT NOW(),
+    dni VARCHAR(9) NOT NULL UNIQUE
+    );
+
+
+CREATE TABLE vehicles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id BINARY(16) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL,
+    year YEAR NOT NULL,
+    type ENUM('Sedan', 'SUV', 'Truck', 'Sports', 'Hatchback', 'Convertible') NOT NULL,
+    transmission ENUM('Manual', 'Automatic') NOT NULL,
+    fuel_type ENUM('Gasoline', 'Diesel', 'Electric', 'Hybrid') NOT NULL,
+    capacity INT NOT NULL,
+    num_doors INT NOT NULL,
+    daily_price DECIMAL(10,2) NOT NULL,
+    deposit DECIMAL (10,2) default 0,
+    availability BOOLEAN DEFAULT TRUE, 
+    registration_date TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE reservations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    vehicle_id INT NOT NULL,
+    customer_id BINARY(16) NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status ENUM('Pending', 'Confirmed', 'Cancelled', 'Completed') DEFAULT 'Pending',
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (vehicle_id) REFERENCES vehicles(id) ON DELETE CASCADE,
+    FOREIGN KEY (customer_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE incidences (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    from_id BINARY(16) NOT NULL,
+    to_id BINARY(16) NULL,
+    reservation_id INT NULL,
+    description TEXT NOT NULL,
+    type ENUM('USER', 'PLATFORM') NOT NULL,
+    status ENUM('Pending', 'In Review', 'Resolved', 'Dismissed') DEFAULT 'Pending',
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (from_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (reservation_id) REFERENCES reservations(id) ON DELETE CASCADE
+);
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    from_id BINARY(16) NOT NULL,
+    to_id BINARY(16) NOT NULL,
+    content TEXT NOT NULL,
+    status ENUM('Sent', 'Delivered', 'Read') DEFAULT 'Sent',
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (from_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE payments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    reservation_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL, -- Monto del pago
+    payment_method ENUM('Credit Card', 'Debit Card', 'PayPal', 'Bank Transfer', 'Cash') NOT NULL, -- Método de pago
+    payment_status ENUM('Pending', 'Completed', 'Failed', 'Refunded') DEFAULT 'Pending', -- Estado del pago
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    transaction_id VARCHAR(255) NULL,
+    FOREIGN KEY (reservation_id) REFERENCES reservations(id) ON DELETE CASCADE
+);
+
+
+-- Insertar usuarios
+
+INSERT INTO users (id, name, email, password, address, phone, role, dni) 
+VALUES (UUID_TO_BIN('123e4567-e89b-12d3-a456-426614174000'),'John Doe', 'john.doe@example.com','1234', '123 Main St, Cityville', '123456790', 'user', '12345678B'),
+(UUID_TO_BIN('234e4567-e89b-12d3-a456-426614174111'),'Alice Smith', 'alice.smith@example.com','4567', '456 Oak Ave, Townsville', '098765321', 'admin', '23456789A'),
+(UUID_TO_BIN('345e4567-e89b-12d3-a456-426614174222'),'Bob Johnson', 'bob.johnson@example.com','124421', '789 Pine Rd, Villagetown', '112334455', 'user', '34567890J');
+
+-- Insertar vehiculos
+
+INSERT INTO vehicles (owner_id, brand, model, year, type, transmission, fuel_type, capacity, num_doors, daily_price, availability) 
+VALUES 
+(UUID_TO_BIN('123e4567-e89b-12d3-a456-426614174000'), 'Hyundai', 'Tucson', 2022, 'Sedan', 'Automatic', 'Gasoline', 5, 3, 45.00, TRUE),
+(UUID_TO_BIN('234e4567-e89b-12d3-a456-426614174111'), 'Citroën', 'Saxo', 2006, 'Sports', 'Manual', 'Gasoline', 2, 4, 80.00, TRUE),
+(UUID_TO_BIN('345e4567-e89b-12d3-a456-426614174222'), 'Nissan', 'Qashqai', 2023, 'SUV', 'Automatic', 'Diesel', 7, 5,120.00, TRUE);
+
+-- INSERTAR RESERVAS
+INSERT INTO reservations (vehicle_id, customer_id, start_date, end_date, total_price, status) 
+VALUES 
+(1, UUID_TO_BIN('123e4567-e89b-12d3-a456-426614174000'), '2023-04-01 10:00:00', '2023-04-07 10:00:00', 315.00, 'Confirmed'),
+(2, UUID_TO_BIN('234e4567-e89b-12d3-a456-426614174111'), '2023-05-10 09:00:00', '2023-05-12 09:00:00', 240.00, 'Pending'),
+(2, UUID_TO_BIN('234e4567-e89b-12d3-a456-426614174111'), '2023-05-10 09:00:00', '2023-05-12 09:00:00', 240.00, 'Pending'),
+(3, UUID_TO_BIN('345e4567-e89b-12d3-a456-426614174222'), '2023-06-15 14:00:00', '2023-06-20 14:00:00', 600.00, 'Cancelled');
+
+
+
+
+
+
+
+-- Observamos que se han insertado bien
+SELECT * FROM vehicles_brands;
+SELECT * FROM vehicles_models;
+SELECT * FROM users;
+SELECT * FROM vehicles;
+SELECT * FROM reservations;
