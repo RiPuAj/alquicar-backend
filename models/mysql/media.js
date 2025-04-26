e5105e57debca6b1ed4faa51ddb3e983e13e6467
NicolasReyAlonsoPC2 <NicolasReyAlonso@gmail.com>
2025-04-26 17:54:59 +0100

    ADD FOLDER CREATION

--- models/mysql/media.js
+++ models/mysql/media.js
@@ -1,11 +1,10 @@
 import { handlerDatabaseError } from '../../errors/handler-error.js';
 import { CreateMYSQLConnection } from './mysql-config.js';
-
+import fs from 'fs'
 const conn = await CreateMYSQLConnection.getConncetion();
 export class MediaModel{
     
-    static async create({ input }){
-        console.log(input);    
+    static async create({ input }){   
         try{
             const qry = `CREATE TABLE \`${input.id}\` (id INT AUTO_INCREMENT PRIMARY KEY,
             vehicle_id INT, URL VARCHAR(255), 
@@ -13,6 +12,13 @@ export class MediaModel{
             );
             `
             const res = await conn.query(qry);
+            fs.mkdir(`assets/${input.id}`, { recursive: true }, (err) => {
+            if (err) {
+                console.error('Error creando la carpeta:', err);
+            } else {
+                console.log('Carpeta creada exitosamente');
+            }
+            });
             return res; 
         } catch(e){ 
             console.log(e);
