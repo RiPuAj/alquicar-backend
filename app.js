04ed51965fb36365f90311ae7e2cd686611680ec
Javier Pulido <javipulido15@gmail.com>
2025-04-30 03:33:49 +0100

    Centralizados los export de modelos y rutas en un archivo index.js

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 import express from 'express';
 import http from 'http';
-import http from 'http';
-import { createUserRouter } from './routes/users.js';
-import { UserModel } from './models/mysql/users.js';
-import { createReservationRouter } from './routes/reservations.js';
-import { ReservationModel } from './models/mysql/reservations.js';
-import { createVehicleRouter } from './routes/vehicles.js';
-import { VehicleModel } from './models/mysql/vehicle.js';
-import { createAuthRouter } from './routes/auth.js';
-import { createIncidenceRouter } from './routes/incidences.js';
-import { IncidenceModel} from './models/mysql/incidences.js';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { WebSocketServerCreator } from './sockets/webSocketServerCreator.js';
@@ -18,6 +9,21 @@ import { SocketsModel } from './models/mysql/sockets.js';
 import { authMiddlewareSocket } from './middlewares/auth.js';
 import { createMessagesEvents } from './sockets/messagesEvents.js';
 import { MessagesModel } from './models/mysql/messages.js';
+
+import {
+  createUserRouter,
+  createReservationRouter,
+  createVehicleRouter,
+  createAuthRouter,
+  createIncidenceRouter
+} from './routes/index.js';
+
+import {
+  UserModel,
+  ReservationModel,
+  VehicleModel,
+  IncidenceModel
+} from './models/mysql/index.js';
 
 dotenv.config({ path: './.env' });
 
@@ -30,16 +36,12 @@ app.use(cors({
   credentials: true,
 }));
 app.disable('x-powered-by');
-app.use(cors({
-  origin: true,
-  credentials: true, 
-}));
 
-app.use('/users', createUserRouter({userModel: UserModel}));
-app.use('/reservations', createReservationRouter({reservationModel: ReservationModel}));
-app.use('/vehicles', createVehicleRouter({vehicleModel: VehicleModel}));
-app.use('/auth', createAuthRouter({userModel: UserModel}));
-app.use('/incidences', createIncidenceRouter({incidenceModel: IncidenceModel}))
+app.use('/users', createUserRouter({ userModel: UserModel }));
+app.use('/reservations', createReservationRouter({ reservationModel: ReservationModel }));
+app.use('/vehicles', createVehicleRouter({ vehicleModel: VehicleModel }));
+app.use('/auth', createAuthRouter({ userModel: UserModel }));
+app.use('/incidences', createIncidenceRouter({ incidenceModel: IncidenceModel }))
 
 app.use((req, res) => {
   res.status(404).send('<h1>404 Not Found</h1>');
