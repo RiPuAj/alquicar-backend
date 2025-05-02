9a8ba94d38bdfbb57bd661f53ce019f496823d7b
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2025-05-02 11:53:43 +0100

    middlewares creados: 1 verifica si eres admin. 2 verifica si eres admin o dueño de la cuenta en la que intentas hacer cambios)

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import { UserController } from "../controllers/users.js";
 import { authMiddleware } from "../middlewares/auth.js";
+import { adminMiddleware, adminOrSelfMiddleware } from "../middlewares/admin.js";
 
 export const createUserRouter = ({userModel}) => {
 
     const userRouter = Router();
     const userController = new UserController({userModel});
 
-    userRouter.get("/", userController.getAll);
+    userRouter.get("/", [authMiddleware, adminMiddleware] , userController.getAll);
     userRouter.get("/getdata", userController.getData)
-    userRouter.get("/:id", authMiddleware, userController.getById);
+    userRouter.get("/:id", [authMiddleware, adminOrSelfMiddleware], userController.getById);
     userRouter.get("/email/:email", userController.getByEmail);
     userRouter.post("/", userController.create);
-    userRouter.patch("/:id", authMiddleware, userController.update);
-    userRouter.delete("/:id", authMiddleware, userController.delete);
+    userRouter.patch("/:id", [authMiddleware, adminOrSelfMiddleware], userController.update);
+    userRouter.delete("/:id", [authMiddleware, adminOrSelfMiddleware], userController.delete);
 
     
 
