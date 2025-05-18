1bdba916c6b67f2bd10d02670eb574f08dc8db7c
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2025-05-18 11:53:25 +0100

    notificationEvents creado

---
+++ sockets/notificationsEvents.js
@@ -0,0 +1,24 @@
+import { SocketsController } from "../controllers/socket.js";
+import { NotificationController } from "../controllers/notifications.js";
+
+export const createNotificationsEvents = ({ io, socketModel, notificationModel }) => {
+    const socketController = new SocketsController({ socketModel: socketModel });
+    const notificationController = new NotificationController({ notificationModel: notificationModel });
+
+    io.on("connection", (socket) => {
+
+        socket.on("get notifications", async () => {
+            io.to(socket.id).emit("get notifications", {
+                notifications: await notificationController.getById({ id: socket.user_info.id })
+            });
+        });
+
+        socket.on("read notification", async (data) => {
+            io.to(socket.id).emit("read notification", {
+                notifications: await notificationController.update({ id: data.id, input: { seen: true } })
+            });
+        });
+
+    });
+
+}
